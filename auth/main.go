0972c8a78faf6b1e90e611a4f445836681b6ee89
Steve King Jr <stevekx86@gmail.com>
2017-03-19 17:25:10 -0700

    Decode JSON

--- auth/main.go
+++ auth/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/login", &LoginHandler{})
+	http.HandleFunc("/login", LoginHandler)
 	http.ListenAndServe(":8000", nil)
 }
 
-type LoginHandler struct {
+type loginCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-func (h *LoginHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func DecodeJSON(src io.Reader, dst interface{}) error {
+	decoder := json.NewDecoder(src)
+	err := decoder.Decode(dst)
+	return err
+}
+
+func LoginHandler(writer http.ResponseWriter, request *http.Request) {
+	x := &loginCredentials{}
+	err := DecodeJSON(request.Body, x)
 	if request.Method != "POST" {
 		writer.WriteHeader(http.StatusMethodNotAllowed)
+	} else if err != nil {
+		io.WriteString(writer, err.Error()+"\n")
 	} else {
-		io.WriteString(writer, "Goodbye world")
+		io.WriteString(writer, x.Username+"\n")
+		io.WriteString(writer, x.Password+"\n")
 	}
 }
