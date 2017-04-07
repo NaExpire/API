6362bf4dad89e8ef0c7d852ceffc84e4e79b49c4
Steve King Jr <stevekx86@gmail.com>
2017-04-07 16:28:08 -0700

    Email validation + mail confirmation code

--- src/business/register.go
+++ src/business/register.go
@@ -3,11 +3,20 @@ package business
 import (
 	"database/sql"
 	"io"
+	"math/rand"
 	"net/http"
+	"net/mail"
+	"net/smtp"
+	"strconv"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+const senderEmail = "fill me in"
+const senderPassword = "fill me in"
+const smtpHost = "smtp.gmail.com"
+const smtpPort = 587
 
 type RegistrationHandler struct {
 	DB *sql.DB
@@ -53,11 +62,28 @@ func (handler RegistrationHandler) ServeHTTP(writer http.ResponseWriter, request
 	}
 
 	// validate phone and address
+	confirmationCode := randomConfirmationCode()
+	if !validatePassword(x.Password) {
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		io.WriteString(writer, "Password does not pass validation")
+	}
+	if !validatePhoneNumber(x.BusinessPhoneNumber) {
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		io.WriteString(writer, "Phone number does not pass validation")
+	}
+	emailValidated, err := validateEmail(x.Email, confirmationCode)
+	if !emailValidated {
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		io.WriteString(writer, "Email is not valid")
+	} else if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(writer, "Could not send confirmation email")
+	}
 
 	registrationDate := time.Now()
 	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(x.Password), 14)
 
-	result, err := handler.DB.Exec("INSERT INTO users (email, password, firstname, lastname, `registration-date`) VALUES (?, ?, ?, ?, ?)", x.Email, string(passwordHash), x.FirstName, x.LastName, registrationDate)
+	result, err := handler.DB.Exec("INSERT INTO users (email, password, firstname, lastname, `registration-date`, `confirmation-code`) VALUES (?, ?, ?, ?, ?, ?)", x.Email, string(passwordHash), x.FirstName, x.LastName, registrationDate, confirmationCode)
 
 	if err != nil {
 		io.WriteString(writer, err.Error()+"\n")
@@ -74,4 +100,34 @@ func (handler RegistrationHandler) ServeHTTP(writer http.ResponseWriter, request
 	}
 
 	io.WriteString(writer, "{\"ok\": true}")
+}
+
+func randomConfirmationCode() int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(89999999) + 10000000
+}
+
+func validatePassword(password string) bool {
+	return true
+}
+
+func validatePhoneNumber(phoneNumber string) bool {
+	return true
+}
+
+func validateEmail(email string, confirmationCode int) (bool, error) {
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		return false, nil
+	}
+	auth := smtp.PlainAuth("", senderEmail, senderPassword, "smtp.gmail.com")
+	msg := "From: " + senderEmail + "\n" +
+		"To: " + email + "\n" +
+		"Subject: NAExpire Registration\n\n" +
+		"Hello! Your confirmation code is " + strconv.Itoa(confirmationCode) + "."
+	err = smtp.SendMail(smtpHost+":"+strconv.Itoa(smtpPort), auth, senderEmail, []string{email}, []byte(msg))
+	if err != nil {
+		return true, err
+	}
+	return true, nil
 }
