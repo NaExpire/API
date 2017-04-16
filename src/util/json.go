383e4f36649b88789333bef1ee3c447fee63ca58
Steve King Jr <stevekx86@gmail.com>
2017-04-15 22:24:18 -0700

    Add utility function for writing failure JSON msgs

--- src/util/json.go
+++ src/util/json.go
@@ -18,3 +18,10 @@ func EncodeJSON(dst io.Writer, src interface{}) error {
 	err := encoder.Encode(src)
 	return err
 }
+
+func WriteErrorJSON(dst io.Writer, errorMsg string) error {
+	response := make(map[string]interface{})
+	response["ok"] = false
+	response["failureReason"] = "Request does not have the header \"Content-Type: application-json\""
+	return EncodeJSON(dst, response)
+}
